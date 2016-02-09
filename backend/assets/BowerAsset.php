64505ac8358e0baf585ee16f4a8211d05aa97659
vlado <sfera66@bk.ru>
2016-02-09 23:54:55 +0200

    adminlte-admin

--- backend/assets/BowerAsset.php
+++ backend/assets/BowerAsset.php
@@ -8,11 +8,6 @@ class BowerAsset extends AssetBundle
     public $sourcePath = '@bower';
     public $css = [
         //'fontawesome/css/font-awesome.min.css', //"Шрифтовые иконки для использования с Twitter Bootstrap"
-
-
-        //'bootstrap/css/bootstrap.min.css',
-       // 'backend/dist/css/AdminLTE.min.css',
-       // 'backend/dist/css/skins/_all-skins.min.css',
        // 'backend/plugins/iCheck/flat/blue.css', //ПЛАГИН ДЛЯ СОЗДАНИЯ ЧЕКБОКСОВ И РАДИО КНОПОК
         //'backend/plugins/morris/morris.css', //Хорошо оформленные диаграммы
     ];
